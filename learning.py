9819db9741827dce6deda767328db693ce9cc7fa
tonijoy <joy.subiri@gmail.com>
2024-08-11 21:09:09 +0100

    added new data about my python learning experience and also a code

--- learning.py
+++ learning.py
@@ -1,1 +1,8 @@
 print('this is my python program')
+print('wow, python is such an exciting data language')
+print('i wish to spend more time mastering my python skills')
+print('lemme show you a little python code that i learnt in the past')
+a= 3
+b=5
+print(a*b)
+      
