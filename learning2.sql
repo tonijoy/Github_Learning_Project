a2df22129c23c63273527910d33bf7f2c32ea336
tonijoy <joy.subiri@gmail.com>
2024-08-11 20:13:02 +0100

    i created a second sql file with some codes

---
+++ learning2.sql
@@ -0,0 +1,7 @@
+SELECT first_name ||' '|| last_name AS full_name, email, address, phone, city, country, sum(amount) AS total_purchase
+FROM customer cs
+JOIN address ad
+ON cs.address_id = ad.address_id
+JOIN city as ct
+ON ad.city_id = ct.city_id
+JOIN country cy
